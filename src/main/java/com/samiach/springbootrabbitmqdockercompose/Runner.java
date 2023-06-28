3ce57e8c569f3d7c2b6890a52a4b420dacf0a42f
samarasena-iach <C5lus9tering$>
2023-06-28 15:41:59 +0530

    Initial Commit

---
+++ src/main/java/com/samiach/springbootrabbitmqdockercompose/Runner.java
@@ -0,0 +1,25 @@
+package com.samiach.springbootrabbitmqdockercompose;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class Runner implements CommandLineRunner {
+    private final RabbitTemplate rabbitTemplate;
+    private final Receiver receiver;
+
+    public Runner(Receiver receiver, RabbitTemplate rabbitTemplate) {
+        this.receiver = receiver;
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Sending message...");
+        rabbitTemplate.convertAndSend(Application.topicExchangeName, "foo.bar.baz", "Hello, RabbitMQ!");
+        receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+    }
+}
---
+++ src/test/java/com/samiach/springbootrabbitmqdockercompose/SpringbootRabbitmqDockercomposeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samiach.springbootrabbitmqdockercompose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRabbitmqDockercomposeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
